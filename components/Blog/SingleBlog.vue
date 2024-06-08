8443036349de1dea2705d10f41add4e41b2617ff
Solomon Eseme <kaperskyguru@gmail.com>
2024-06-08 09:27:00 +0100

    fix: update link

--- components/Blog/SingleBlog.vue
+++ components/Blog/SingleBlog.vue
@@ -5,7 +5,7 @@
     class="wow fadeInUp hover:shadow-two dark:hover:shadow-gray-dark group relative overflow-hidden rounded-sm bg-white shadow-one duration-300 dark:bg-dark"
     data-wow-delay=".1s"
   >
-    <a :href="blog.link" class="relative block w-full">
+    <a :href="blog.link" target="_blank" class="relative block w-full">
       <span
         class="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white"
       >
@@ -16,6 +16,7 @@
     <div class="p-6 sm:p-8 md:px-6 md:py-8 lg:p-8 xl:px-5 xl:py-8 2xl:p-8">
       <h3>
         <a
+          target="_blank"
           :href="blog.link"
           class="mb-4 block text-md font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-xl"
         >
