327b6666b9285f64560d4b25901fd5437bb4121d
Solomon Eseme <kaperskyguru@gmail.com>
2025-12-15 15:07:42 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -10,5 +10,6 @@
     <div id="portal-dialog"></div>
     <div id="portal-tooltip"></div>
     <div id="portal-toast"></div>
+    <script src="https://topbanner.app/api/embed/a58ccbb1649c93b9c511fe5530e38570.js" async></script>
   </div>
 </template>
